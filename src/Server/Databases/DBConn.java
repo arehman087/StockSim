aeed940777ac7803944d0e7d70aac9ece829ad69
Abdul Rehman <abudrehman45@gmail.com>
2019-02-08 13:41:01 -0600

    Created Empty functions as well as the connection to the DB

--- src/Server/Databases/DBConn.java
+++ src/Server/Databases/DBConn.java
@@ -1,1 +1,64 @@
-//add api Code here!
+//add DB Code here!
+package Server.Databases;
+
+import java.sql.*;
+
+class DBConn{
+    private String driver;
+    private String url;
+
+    private String user;
+    private String pass;
+
+    private Connection conn;
+
+    /**
+     * Makes an instance of the db to connect to it.
+     */
+    public DBConn(){
+
+        this.driver = "org.postgresql.Driver";
+        this.url = "jdbc:postgresql://localhost:5432/StockSim";
+
+        this.user = "postgres";
+        this.pass = "Passcode1";
+
+    }
+
+    public void openConn(){
+        try{
+            Class.forName(this.driver);
+            System.out.println("connecting to db");
+            this.conn = DriverManager.getConnection(this.url, this.user, this.pass);
+        }
+        catch (Exception e){
+            System.out.print(e.getMessage());
+        }
+    }
+
+    public void closeConn(){
+        try{
+            conn.close();
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void insertUser(){
+
+    }
+
+    public void insertWatchList(){
+
+    }
+
+    public void insertTransaction(){
+
+    }
+
+    //returns Results from db
+    public ResultSet getData(){
+        return null;
+    }
+}
